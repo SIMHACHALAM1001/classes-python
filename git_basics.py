e98780109474080f729dcd4a0b2e08a025fa42b5
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2026-06-30 05:19:29 +0530

    AB#123:git basics

--- git_basics.py
+++ git_basics.py
@@ -16,4 +16,14 @@
 # main --------> git_steps
 
 
+# how to add the files to github(remote which is in github.com)
+# git add .
+# how to commit the new files or changes any code in to github
+# git commit -m "AB#123:git basics"
+# how to push the code from your local system newely created branch to remote repository as a new branch
+# git push origin branch_name (ex: git_basics)
+
+
+
+
 
