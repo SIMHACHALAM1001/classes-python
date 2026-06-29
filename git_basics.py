d2946089c528be1c4fe7f8c8ce541e958c84ec0e
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2026-06-30 05:16:35 +0530

    AB#123:git basics

---
+++ git_basics.py
@@ -0,0 +1,19 @@
+# clone the project
+# git clone url
+# how to check currently which branch you are in 
+# git status (python_basics (main))
+# how to create a new branch
+# git checkout -b branch-name(ex:git_basics)
+#  check at which you are in
+# git status
+# how to checkout from one branch to another branch
+# git checkout branch_name
+
+
+#                 from
+# python_basics ----------> git_basics ---> git status
+#       from
+# main --------> git_steps
+
+
+
