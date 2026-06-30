30e8f74543995e80c4ad201f19dd60d133a8b30a
Kaushik-Shetti-Kilowott <kaushik.shetti@kilowott.com>
2026-06-30 05:41:05 +0530

    AB#123:git stash concept

--- git_basics.py
+++ git_basics.py
@@ -23,7 +23,26 @@
 # how to push the code from your local system newely created branch to remote repository as a new branch
 # git push origin branch_name (ex: git_basics)
 
+# 3 team members
+# main branch ------> complete branch --> payments code 
+# dwarkesh ---> orders branch (no payments code)---> orderbranch --> still he is working 
+# kartheya ---> payment branch --> pull request --> resolved pr comments --> merge main branch
+# devlopers---> pushed the code ---> merged 
 
+# every day your branch must be updated with main branch code
+# git pull origin branch_name(ex : main)
+# git stash 
 
+# dwarkesh ---> orders(pulled code main branch)   ---> only one file --> there is a chance of conflits, both are changing on single function
+# ---> 
 
+# kartheya ---> payment orders --> only one file ---> merged --> main
+# dwarkesh
+# need to pull the latest code from main branch
+# git stash ---> save orders code in memory
+# git status --> empth there is no changed files
+# git pull origin main
+# dwarkesh has updated code because he pulled the code from main branch
+# doesn't have order's code & that code is in stash list(memory)
+# git stash apply stash@{0} / git stash apply (recent stash will apply here)
 
